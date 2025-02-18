aa2702f2f8c2ce0746583aefe79c11245f8afc89
Shahid <shahidsaifi138@gmail.com>
2025-02-18 15:39:09 +0530

    Connect to DB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import mongoose from "mongoose"; // always use try catch and async await in database connections.
-import mydb, { DB_NAME } from "./constants.js";
+// require('dotenv').config({path: './env'});
+
 import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
 
 const app = express();
 
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+});
+connectDB();
+
+
+
+
+
+// import mongoose from "mongoose"; // always use try catch and async await in database connections.
+// import { DB_NAME } from "./constants.js";
+/*
 (async () => {
     try {
         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, )
@@ -16,6 +39,6 @@ const app = express();
         })
     } catch (error) {
         console.log("ERROR: ", error)
-        throw err
+        throw error
     }
-})()
+})() */
